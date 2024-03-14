e6df28970f935ab5e38032301a46a0ce4cf04df9
Amariah <amariah.abish@gmail.com>
2024-03-14 14:26:21 +0300

    Js event Listener

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
-}
+    // Grab the element
+    const input = document.getElementById('button');
+  
+    // Define a named function to handle the click event
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+  
+    // Add event listener to the element using the named function
+    input.addEventListener('click', clickAlert);
+  }
+  
